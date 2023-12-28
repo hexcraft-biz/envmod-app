73a72179595c290f9465d10b593fbd56f2d6d95b
Boss <cure1218@gmail.com>
2023-12-28 08:47:43 +0000

    boss

--- core.go
+++ core.go
@@ -4,7 +4,6 @@ import (
 	"net/url"
 	"os"
 	"path"
-	"strconv"
 	"time"
 )
 
@@ -12,15 +11,14 @@ import (
 //
 // ================================================================
 type App struct {
-	AppRootUrl    *url.URL
-	AppTitle      string
-	AppHost       string
-	AppPath       string
-	AppPort       string
-	GinMode       string
-	Location      *time.Location
-	OAuth2Enabled bool
-	TrustProxy    string
+	AppRootUrl *url.URL
+	AppTitle   string
+	AppHost    string
+	AppPath    string
+	AppPort    string
+	GinMode    string
+	Location   *time.Location
+	TrustProxy string
 }
 
 // ================================================================
@@ -32,20 +30,14 @@ func New() (*App, error) {
 		return nil, err
 	}
 
-	oAuth2Enabled, err := strconv.ParseBool(os.Getenv("OAUTH2_ENABLED"))
-	if err != nil {
-		return nil, err
-	}
-
 	env := &App{
-		AppTitle:      os.Getenv("APP_TITLE"),
-		AppHost:       os.Getenv("APP_HOST"),
-		AppPath:       path.Join("/", os.Getenv("APP_PATH")),
-		AppPort:       os.Getenv("APP_PORT"),
-		GinMode:       os.Getenv("GIN_MODE"),
-		Location:      loc,
-		OAuth2Enabled: oAuth2Enabled,
-		TrustProxy:    os.Getenv("TRUST_PROXY"),
+		AppTitle:   os.Getenv("APP_TITLE"),
+		AppHost:    os.Getenv("APP_HOST"),
+		AppPath:    path.Join("/", os.Getenv("APP_PATH")),
+		AppPort:    os.Getenv("APP_PORT"),
+		GinMode:    os.Getenv("GIN_MODE"),
+		Location:   loc,
+		TrustProxy: os.Getenv("TRUST_PROXY"),
 	}
 
 	env.AppRootUrl, err = url.ParseRequestURI("https://" + path.Join(env.AppHost, env.AppPath))
